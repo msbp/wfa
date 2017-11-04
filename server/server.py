a20425b4b9c18496a176ed6112bdb7f6c8c16a8f
James <jamesleahy314@gmail.com>
2017-11-04 01:10:09 -0700

    reorganize files

---
+++ server/server.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+import sys
+sys.path.append('/libs/decisiontreeExample')
+import decisiontree
+
+app = Flask(__name__)
+
+@app.route("/")
+def test():
+    return "test"
+
+@app.route("/tree")
+def tree():
+    x = decisiontree.DecisionTree()
+    mytree = x.createDecisionTreeModel()
+    
+    return str(mytree)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
