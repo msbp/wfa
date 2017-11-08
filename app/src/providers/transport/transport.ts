aa70b769cdd1525c827c130a592672a5ffe533be
James <jamesleahy314@gmail.com>
2017-11-07 23:41:01 -0800

    implement get

--- app/src/providers/transport/transport.ts
+++ app/src/providers/transport/transport.ts
@@ -1,36 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HTTP } from '@ionic-native/http';
+import { HTTP} from '@ionic-native/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the TransportProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class TransportProvider {
+  
+BASE_URL = "https://wfa-server.herokuapp.com/"
+
 
   constructor(private http: HTTP) {
-    console.log('Hello TransportProvider Provider');
+
     }
 
-  get() {
-
-    console.log("Making get request!")
-    this.http.get('https://wfa-server.herokuapp.com', {}, {}).then(data => {
-        console.log("success: " + data.status)
-        console.log("here is the data: " + data.data)
-
-
-
-
-        }).catch(error => {
-                console.log("error: " + JSON.stringify(error.status));
-
-
-
+  get(endpoint: string) {
+        return new Promise((resolve, reject) => {
+            this.http.get(this.BASE_URL + endpoint, {}, {}).then(data => {
+                         resolve(data.data)
+                }).catch(error => {
+                         reject(error)
+                });
         });
+  }
+
+  post(endpoint: string) {
 
 
   }
