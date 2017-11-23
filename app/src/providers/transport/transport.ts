f09b402339914c6f7b3107614c4410ee26777aa6
James <jamesleahy314@gmail.com>
2017-11-23 00:51:13 -0800

    working post

--- app/src/providers/transport/transport.ts
+++ app/src/providers/transport/transport.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HTTP} from '@ionic-native/http';
+import {Headers, RequestOptions, Response, Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import {Observable} from "rxjs/Rx";
 
 
 @Injectable()
@@ -9,23 +12,41 @@ export class TransportProvider {
 BASE_URL = "https://wfa-server.herokuapp.com/"
 
 
-  constructor(private http: HTTP) {
+  constructor(private http: Http) {
 
     }
 
   get(endpoint: string) {
-        return new Promise((resolve, reject) => {
+            return new Promise((resolve, reject) => {
             this.http.get(this.BASE_URL + endpoint, {}, {}).then(data => {
                          resolve(data.data)
                 }).catch(error => {
                          reject(error)
                 });
-        });
+        }); 
   }
 
   post(endpoint: string) {
+    let body = JSON.stringify({
+        example: 100
+      });
 
+    let headers = new Headers({
+      'Content-Type': "application/json"
+    });
+    let options = new RequestOptions({
+      headers: headers
+    });
+    this.http.post(this.BASE_URL + endpoint, body, options).toPromise().then(response => 
+     this.processResponse(response), this.handleError);  
+  }
 
+  processResponse(response) {
+    console.log("this is the response:" + JSON.stringify(response));
+  }
+
+  handleError(error: Response | any) {
+    console.log(error.status);
   }
 
 }
