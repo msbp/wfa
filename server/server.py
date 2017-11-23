fdc786ebecdf594584cd31f94fb71192c6cc73f0
James <jamesleahy314@gmail.com>
2017-11-23 00:29:26 -0800

    attempt to resolve cors issue

--- server/server.py
+++ server/server.py
@@ -1,10 +1,13 @@
 import os
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import sys
 sys.path.append('/app/server/libs/decisiontreeExample/')
 import decisiontree
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config('CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def test():
@@ -18,6 +21,7 @@ def tree():
     return str(mytree)
 
 @app.route("/examplePost", methods=['POST'])
+@cross_origin()
 def postExample():
     json = request.get_json(force=True)
     return str(json['example'])
