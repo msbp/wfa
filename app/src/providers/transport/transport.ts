1e9b2004638a27d33e17a9828361af086f3efeab
WENYUNLIAO <wenyun@WENYUNLIAOs-MBP.hitronhub.home>
2017-11-24 23:58:52 -0800

    format fixed

--- app/src/providers/transport/transport.ts
+++ app/src/providers/transport/transport.ts
@@ -1,42 +1,20 @@
 import { Injectable } from '@angular/core';
-import {Headers, RequestOptions, Response, Http} from '@angular/http';
+import { Headers, RequestOptions, Response, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-import {Observable} from "rxjs/Rx";
+import { Observable } from "rxjs/Rx";
 
 
 @Injectable()
 export class TransportProvider {
-  
-BASE_URL = "https://wfa-server.herokuapp.com/"
+
+  BASE_URL = "https://wfa-server.herokuapp.com/"
 
 
   constructor(private http: Http) {
 
-    }
-
-  get(endpoint: string) {
-
-           let headers = new Headers({
-          'Content-Type': "application/json"
-            });
-            let options = new RequestOptions({
-            headers: headers
-            });
-
-            return new Promise((resolve, reject) => {
-            this.http.get(this.BASE_URL + endpoint, options).toPromise().then(data => {
-                         resolve(data["_body"])
-                }).catch(error => {
-                         reject(error)
-                });
-        }); 
   }
 
-  post(endpoint: string) {
-    let body = JSON.stringify({
-        example: 100
-      });
-
+  get(endpoint: string) {
     let headers = new Headers({
       'Content-Type': "application/json"
     });
@@ -45,11 +23,29 @@ BASE_URL = "https://wfa-server.herokuapp.com/"
     });
 
     return new Promise((resolve, reject) => {
-    this.http.post(this.BASE_URL + endpoint, body, options).toPromise().then(response => {
-      resolve(JSON.stringify(response))
-    }).catch(error => {
+      this.http.get(this.BASE_URL + endpoint, options).toPromise().then(data => {
+        resolve(data["_body"])
+      }).catch(error => {
+        reject(error)
+      });
+    });
+  }
+
+  post(endpoint: string, data) {
+    let body = data
+    let headers = new Headers({
+      'Content-Type': "application/json"
+    });
+    let options = new RequestOptions({
+      headers: headers
+    });
+
+    return new Promise((resolve, reject) => {
+      this.http.post(this.BASE_URL + endpoint, body, options).toPromise().then(response => {
+        resolve(response["_body"])
+      }).catch(error => {
         reject(error)
       })
-    }) 
+    })
   }
 }
