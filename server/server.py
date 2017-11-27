5e07e759d9f9961df6052cb0cb74631e33e4b14a
James <jamesleahy314@gmail.com>
2017-11-27 12:14:54 -0800

    working model

--- server/server.py
+++ server/server.py
@@ -3,7 +3,9 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import sys
 sys.path.append('/app/server/libs/decisiontreeExample/')
+sys.path.append('/app/server/libs/decisionTree')
 import decisiontree
+import prediction
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -26,6 +28,16 @@ def postExample():
     json = request.get_json(force=True)
     return str(json['example'])
 
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predict():
+    json = request.get_json(force=True)
+    longitude = json['longitude']
+    latitude = json['latitude']
+    year = json['year']
+    s = prediction.Prediction()
+    pred = s.predict(year, longitude, latitude)
+    return str(pred)
 
 
 if __name__ == "__main__":
