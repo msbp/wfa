894e11d03b41cd614bd2307ffc265f3f3270f1b6
James <jamesleahy314@gmail.com>
2017-11-27 16:41:45 -0800

    fix order of params

--- server/libs/decisionTree/prediction.py
+++ server/libs/decisionTree/prediction.py
@@ -26,7 +26,7 @@ class Prediction():
         print year
         print lat
         print lon
-        value = clf_gini.predict([[int(year), int(lat), int(lon)]])
+        value = clf_gini.predict([[int(year), int(lon), int(lat)]])
         print value
         
 
